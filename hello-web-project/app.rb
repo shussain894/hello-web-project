daaca253ce64d50ebc7d4cd7c35d46f8cc7baab0
shahrukh hussain <shah@MASTUDENT-shahrukh-hussain-C17GMB23Q6L4.local>
2022-11-28 15:49:04 +0000

    first commit for web

---
+++ hello-web-project/app.rb
@@ -0,0 +1,18 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/names' do
+    name = params[:name]
+  
+    return "#{name}"
+  end 
+
+end
+
